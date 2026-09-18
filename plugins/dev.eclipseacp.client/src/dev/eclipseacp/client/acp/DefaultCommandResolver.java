7e03e80233b09f2303b5f253dbe0310bb407dbdd
ivanrododendro <35653122+ivanrododendro@users.noreply.github.com>
2026-09-18 14:48:38 +0200

    Remove unused resolver import

--- plugins/dev.eclipseacp.client/src/dev/eclipseacp/client/acp/DefaultCommandResolver.java
+++ plugins/dev.eclipseacp.client/src/dev/eclipseacp/client/acp/DefaultCommandResolver.java
@@ -2,7 +2,6 @@ package dev.eclipseacp.client.acp;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
